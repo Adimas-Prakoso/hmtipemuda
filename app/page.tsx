2c2f16a00ca2ee780d3f462e187ee4e2679074fc
Adimas Prakoso <adimas.prakoso090704@outlook.com>
2025-02-28 02:12:55 +0700

    up

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,13 @@ import Navbar from './components/Navbar';
 import AboutSection from './components/AboutSection';
 import ProgramHighlightsSection from './components/ProgramHighlightsSection';
 import LeadershipSection from './components/LeadershipSection';
-
-const slides = [
-  { image: '/images/hero1.webp', alt: 'Image 1' },
-  { image: '/images/hero2.webp', alt: 'Image 2' },
-  // { image: '/images/hero3.webp', alt: 'Image 3' },
-];
+import siteData from '@/data/site.json';
 
 export default function Home() {
   return (
     <main>
       <Navbar />
-      <HeroSection slides={slides} />
+      <HeroSection slides={siteData.slides} />
       <AboutSection />
       <ProgramHighlightsSection />
       <LeadershipSection />
