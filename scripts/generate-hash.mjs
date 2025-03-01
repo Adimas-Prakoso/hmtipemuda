b08caaa1e652ce45e6d2a594735d73538d7849d7
Adimas Prakoso <adimas.prakoso090704@outlook.com>
2025-03-01 13:15:30 +0700

    up

--- scripts/generate-hash.mjs
+++ scripts/generate-hash.mjs
@@ -1,11 +1,11 @@
 import bcrypt from 'bcryptjs';
 
-const password = 'admin123';
+const password = '{[a=p$6/MVw:8Ap';
 const saltRounds = 10;
 
 try {
     const hash = await bcrypt.hash(password, saltRounds);
-    console.log('Generated hash for password "admin123":', hash);
+    console.log(`Generated hash for password ${password}:`, hash);
 } catch (err) {
     console.error('Error generating hash:', err);
 }
